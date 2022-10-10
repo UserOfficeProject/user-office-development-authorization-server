73f7bf0f8ff8dc2490a58a4263d983c892a7ec10
jekabskarklins <jekabs.karklins@ess.eu>
2022-10-10 11:34:20 +0200

    fix: update issuer name

--- src/index.js
+++ src/index.js
@@ -11,19 +11,22 @@ const Account = require('./models/Account');
 const configuration = require('./config/config');
 const routes = require('./routes/routes');
 
-const { PORT = 5000, ISSUER = `http://localhost:${PORT}` } = process.env;
+const { PORT = 5000, ISSUER = `user-office-development-authorization-server` } =
+  process.env;
 configuration.findAccount = Account.findAccount;
 
 const app = express();
 
 const directives = helmet.contentSecurityPolicy.getDefaultDirectives();
 delete directives['form-action'];
-app.use(helmet({
-  contentSecurityPolicy: {
-    useDefaults: false,
-    directives,
-  },
-}));
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      useDefaults: false,
+      directives,
+    },
+  })
+);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -38,7 +41,9 @@ let server;
 
   app.use(provider.callback());
   server = app.listen(PORT, () => {
-    console.log(`application is listening on port ${PORT}, check its /.well-known/openid-configuration`);
+    console.log(
+      `application is listening on port ${PORT}, check its /.well-known/openid-configuration`
+    );
   });
 })().catch((err) => {
   if (server && server.listening) server.close();
