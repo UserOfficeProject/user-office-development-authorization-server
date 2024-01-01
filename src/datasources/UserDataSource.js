836e0f36bdf60b9887832ce77b57d69608a4c185
Jekabs Karklins <enthusiast.studio@gmail.com>
2024-01-01 19:57:17 +0100

    fix: getInstitutionColumnName that supports old and new column name

--- src/datasources/UserDataSource.js
+++ src/datasources/UserDataSource.js
@@ -1,25 +1,45 @@
 const db = require('./connection');
 
 class UserDataSource {
-  static async getUserByLogin(email, password) { // eslint-disable-line no-unused-vars
+  static async getUserByLogin(email, _password) {
+    const institutionColumnName = await this.getInstitutionColumnName();
     return db
       .select()
       .from('users as u')
-      .join('institutions as i', { 'u.organisation': 'i.institution_id' })
+      .join('institutions as i', {
+        [`u.${institutionColumnName}`]: 'i.institution_id',
+      })
       .where('email', 'ilike', email)
       .first()
-      .then((user) => (user || null))
+      .then((user) => user || null)
       .catch((err) => {
         console.error(err);
         return null;
       });
   }
 
-  static async getUserBySub(sub) { // eslint-disable-line no-unused-vars
+  // this function intermediately uses the old column name for backwards compatibility
+  // please remove once all users have been migrated to the new column name
+  static async getInstitutionColumnName() {
+    const hasOrganisationColumn = await db.schema.hasColumn(
+      'users',
+      'organisation',
+    );
+    if (hasOrganisationColumn) {
+      return 'organisation'; // fallback to old column name for backwards compatibility
+    }
+    return 'institution_id';
+  }
+
+  static async getUserBySub(sub) {
+    // eslint-disable-line no-unused-vars
+    const institutionColumnName = await this.getInstitutionColumnName();
     return db
       .select()
       .from('users as u')
-      .join('institutions as i', { 'u.organisation': 'i.institution_id' })
+      .join('institutions as i', {
+        [`u.${institutionColumnName}`]: 'i.institution_id',
+      })
       .where('oidc_sub', sub)
       .first()
       .then((user) => user || null)
